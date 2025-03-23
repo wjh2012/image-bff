5db577244b77471252eaae4b13d95145e9d3c179
wjh <wjh2012@naver.com>
2025-03-23 23:16:55 +0900

    login dto

---
+++ src/main/kotlin/com/ggomg/imagebff/user/model/login/LoginRequest.kt
@@ -0,0 +1,6 @@
+package com.ggomg.imagebff.user.model.login
+
+data class LoginRequest (
+    val email: String,
+    val password: String,
+)
---
+++ src/main/kotlin/com/ggomg/imagebff/user/model/login/LoginResponse.kt
@@ -0,0 +1,5 @@
+package com.ggomg.imagebff.user.model.login
+
+data class LoginResponse (
+    val success: Boolean,
+)
