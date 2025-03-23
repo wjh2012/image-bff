cf3d4ec6c4094878dc384236a67837dd31b62cc0
wjh <wjh2012@naver.com>
2025-03-23 21:51:58 +0900

    Init

---
+++ src/main/kotlin/com/ggomg/imagebff/ImageBffApplication.kt
@@ -0,0 +1,11 @@
+package com.ggomg.imagebff
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImageBffApplication
+
+fun main(args: Array<String>) {
+    runApplication<ImageBffApplication>(*args)
+}
---
+++ src/test/kotlin/com/ggomg/imagebff/ImageBffApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ggomg.imagebff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImageBffApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
