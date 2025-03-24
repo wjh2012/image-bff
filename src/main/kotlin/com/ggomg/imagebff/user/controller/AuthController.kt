3dab2c208ae3983a2f01e16df206d8fb64708846
wjh <wjh2012@naver.com>
2025-03-24 22:33:13 +0900

    clean

--- src/main/kotlin/com/ggomg/imagebff/user/controller/AuthController.kt
+++ src/main/kotlin/com/ggomg/imagebff/user/controller/AuthController.kt
@@ -18,7 +18,11 @@ class AuthController(
     @PostMapping("/register")
     @Operation(summary = "회원가입", description = "사용자 회원가입을 진행한다.")
     fun normalRegister(registerRequest: RegisterRequest): ResponseEntity<RegisterResponse> {
-        authService.registerNormalUser(registerRequest.name, registerRequest.email, registerRequest.password)
+        authService.registerNormalUser(
+            registerRequest.name,
+            registerRequest.email,
+            registerRequest.password
+        )
         return ResponseEntity.ok(RegisterResponse(true))
     }
 
