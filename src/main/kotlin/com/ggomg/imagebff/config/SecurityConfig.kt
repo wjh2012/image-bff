ae4664f99b3ce18c8d237f231bf4d4c52d1cbbef
wjh <wjh2012@naver.com>
2025-03-25 00:05:47 +0900

    securityConfig oauth2Login

--- src/main/kotlin/com/ggomg/imagebff/config/SecurityConfig.kt
+++ src/main/kotlin/com/ggomg/imagebff/config/SecurityConfig.kt
@@ -2,11 +2,14 @@ package com.ggomg.imagebff.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig {
 
     @Bean
@@ -16,6 +19,7 @@ class SecurityConfig {
             .sessionManagement { session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             }
+            .oauth2Login(Customizer.withDefaults())
             .authorizeHttpRequests { auth ->
                 auth
                     .requestMatchers(
