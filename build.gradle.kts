8220dc29ce37a35055af8b6ccd6a33ce5a832a12
wjh <wjh2012@naver.com>
2025-03-27 20:10:17 +0900

    format

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,10 +33,17 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
     compileOnly("org.projectlombok:lombok")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
+
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("com.h2database:h2")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
--- src/main/kotlin/com/ggomg/imagebff/user/repository/UserRepository.kt
+++ src/main/kotlin/com/ggomg/imagebff/user/repository/UserRepository.kt
@@ -4,7 +4,9 @@ import com.ggomg.imagebff.user.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository : JpaRepository<User, Long>, UserRepositoryCustom {
+
     fun findByEmail(email: String): User?
 
     fun existsByEmail(email: String): Boolean
+
 }
