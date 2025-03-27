50fd897363ce52f991ee3e16a93b2d597ffa8be4
wjh <wjh2012@naver.com>
2025-03-27 20:10:48 +0900

    mockk

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,8 +47,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
-    runtimeOnly("org.postgresql:postgresql")
-    runtimeOnly("com.h2database:h2")
+    testImplementation("io.mockk:mockk:1.13.17")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
