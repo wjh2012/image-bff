f3216958e88c111db91b609c4430425dfee3d77f
wjh <wjh2012@naver.com>
2025-03-27 20:08:20 +0900

    securityConfig passwordEncoder

--- src/main/kotlin/com/ggomg/imagebff/config/SecurityConfig.kt
+++ src/main/kotlin/com/ggomg/imagebff/config/SecurityConfig.kt
@@ -6,11 +6,20 @@ import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
+
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
@@ -30,6 +39,8 @@ class SecurityConfig {
                     ).permitAll()
                     .anyRequest().authenticated()
             }
+            .formLogin { it.disable() }
+            .httpBasic { it.disable() }
 
         return http.build()
     }
