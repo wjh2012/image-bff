c3018bddfefb4d1fed55f5eed78d4ee01554cdb7
wjh <wjh2012@naver.com>
2025-06-05 16:05:14 +0900

    spring security logging

--- src/main/kotlin/com/ggomg/imagebff/ImageBffApplication.kt
+++ src/main/kotlin/com/ggomg/imagebff/ImageBffApplication.kt
@@ -3,9 +3,11 @@ package com.ggomg.imagebff
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 class ImageBffApplication
 
 fun main(args: Array<String>) {
