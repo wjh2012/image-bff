1e0e0533c96155adfad39530153ec8bdd20079e4
wjh <wjh2012@naver.com>
2025-06-23 22:36:16 +0900

    rename

--- src/main/kotlin/com/ggomg/imagebff/image/domain/ImageRepository.kt
+++ src/main/kotlin/com/ggomg/imagebff/image/domain/ImageRepository.kt
@@ -5,5 +5,6 @@ import java.util.UUID
 interface ImageRepository {
     fun save(image: Image)
     fun saveAll(images: List<Image>)
-    fun read(id: UUID): Image
+    fun findById(id: UUID): Image?
+    fun findAllByIds(ids: List<UUID>): List<Image>
 }
